6c2b039b8be84aec41889099bb08e52839d4a23a
Alan <av20733@ms.wasatch.edu>
2022-11-21 10:16:45 -0700

    Setup

---
+++ functions.js
@@ -0,0 +1,32 @@
+var buttonThing = document.getElementById("button");
+
+
+
+
+var submit = document.getElementById("submit");
+submit = Submit();
+
+function Submit(){
+    if(submit.click){
+        
+    }else{
+        
+    }
+    
+}
+
+
+
+
+function putToast(){
+
+    window.alert("YOU HAVE CLICK THE MIGHTY BUTTOn");
+    
+
+}
+
+function stylisticChanges(){
+
+    var textbox = document.getElementById("something");
+   
+}
